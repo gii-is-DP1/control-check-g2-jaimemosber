fa20c42f3ee4b4eafd10f857e470b1f05d2ec892
Jaime Moscoso Bernal <jaimesfc@live.com>
2022-01-14 10:13:51 +0100

    Entrega

--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingService.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingService.java
@@ -2,22 +2,36 @@ package org.springframework.samples.petclinic.feeding;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class FeedingService {
+	
+	@Autowired
+	private FeedingRepository feedingRepository;
+	
     public List<Feeding> getAll(){
-        return null;
+        List<Feeding> f = feedingRepository.findAll();
+    	return f;
     }
 
     public List<FeedingType> getAllFeedingTypes(){
-        return null;
+        List<FeedingType> f = feedingRepository.findAllFeedingTypes();
+        return f;
     }
 
     public FeedingType getFeedingType(String typeName) {
-        return null;
+       FeedingType feedingType = feedingRepository.findFeedingRepository(typeName);
+       return feedingType;
     }
 
-    public Feeding save(Feeding p) throws UnfeasibleFeedingException {
-        return null;       
+//    public Feeding save(Feeding p) throws UnfeasibleFeedingException {
+//        return feedingRepository.save(p);  
+//    }
+    
+    public void save (Feeding f) {
+    	feedingRepository.save(f);
     }
-
     
 }
